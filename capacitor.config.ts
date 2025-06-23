e0009845db8eb60319f1587494c30f1f0e3eae8e
nielle003 <niellethebest@gmail.com>
2025-06-23 08:04:49 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TaskMatePro',
+  webDir: 'www'
+};
+
+export default config;
