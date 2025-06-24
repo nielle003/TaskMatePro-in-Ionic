80d626485a6f81e78ad7fcfbb629a9aaaa160b99
nielle003 <niellethebest@gmail.com>
2025-06-24 08:52:48 +0800

    refactor register and login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
+import { LoadingController, ToastController } from '@ionic/angular';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -13,36 +15,70 @@ export class LoginPage {
   password = '';
   errorMessage = '';
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(
+    private authService: AuthService, 
+    private router: Router,
+    private loadingCtrl: LoadingController,
+    private toastCtrl: ToastController
+  ) { }
   
-  onLogin(){
-    const credentials ={
+  async onLogin() {
+    const loading = await this.loadingCtrl.create({
+      message: 'Logging in...',
+      spinner: 'crescent',
+      duration: 3000
+    });
+
+    await loading.present();
+
+    const credentials = {
       email: this.email,
       password: this.password
     };
 
-    console.log(this.email);
-    console.log(this.password);
-   this.authService.login(credentials).subscribe({
-      next: async (res) => {
-        console.log('Backend response:', res);
-        if (res.success) {
-          await this.authService.storeToken(res.token);
-          console.log('saved token:', res.token);
-          this.router.navigate(['/home']);
-        } else {
-          this.errorMessage = res.message || 'Login failed';
-        }
-      },
-      error: (err) => {
-        console.error('Backend error:', err);
-        this.errorMessage = 'Server error';
-      }
-    });
+    try {
+      const res: any = await firstValueFrom(this.authService.login(credentials));
+      await loading.dismiss();
 
+      if (res.success) {
+        await this.authService.storeToken(res.token);
+        console.log('Saved token:', res.token);
+        this.router.navigate(['/home']);
+      } else {
+        this.showToast('Login failed', 'danger');
+        this.errorMessage = res.message || 'Login failed';
+      }
+
+    } catch (err) {
+      await loading.dismiss();
+      this.showToast('Server error occurred!', 'danger');
+      console.error('Backend error:', err);
+      this.errorMessage = 'Server error';
+    }
   }
 
-  onRegister(){
+
+  async onRegister(){
+
+    const go = await this.loadingCtrl.create({
+      message: 'Going to register....',
+      spinner: 'circular',
+      duration: 1000
+
+    })
+    await go.present();
+    await go.dismiss();
     this.router.navigate(['/register']);
+  }
+
+
+  async showToast(message: string, color: string = 'primary'){
+    const toast = await this.toastCtrl.create({
+      message,
+      duration: 2000,
+      color,
+      position: 'top'
+    });
+    toast.present();
   }
 }
--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import { LoadingController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -13,28 +14,51 @@ export class RegisterPage implements OnInit {
   password:string = '';
   message:string = '';
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, 
+    private router: Router,
+    private loadingCtrl: LoadingController,
+    private toastCtrl: ToastController
+  
+  ) { }
 
   ngOnInit() {
   }
 
-  register() {
+  async register() {
+    const loading = await this.loadingCtrl.create({
+      message: 'Registering...',
+      spinner: 'crescent',
+      duration: 3000
+    })
+    await loading.present();
+
+
     this.authService.register( this.email, this.password).subscribe({
-      next: (res: any) => {
+      next: async (res: any) => {
+        await loading.dismiss();
         if (res.success) {
-          alert('Successfully Registered!');
-          this.message = res.message;
+          this.showToast('Successfully Registered!', 'success');
           this.router.navigate(['/login']);
         } else {
-          alert('Registration Failed');
-          console.log(res.error);
+          this.showToast('Registration Failed', 'danger')
         }
       },
-      error: (err:any) => {
-        alert('Something went wrong');
+      error: async (err:any) => {
+        await loading.dismiss();
+        this.showToast('Server error occurred!', 'danger');
         console.log(err);
       }
     });
+  }
+
+  async showToast(message: string, color: string = 'primary'){
+    const toast = await this.toastCtrl.create({
+      message,
+      duration: 2000,
+      color,
+      position: 'top'
+    });
+    toast.present();
   }
 
 }
